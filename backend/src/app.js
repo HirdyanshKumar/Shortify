67c93b0b37c97019ff54ec5dc6f2707b4cdee94d
Hirdyansh Kumar <kumarhirdyansh25@gmail.com>
2025-12-30 15:11:12 +0530

    Change CORS origin to allow all origins

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://shortify-eta.vercel.app"],
+  origin: "*",
   credentials: true
 }));
 app.use(express.json());
